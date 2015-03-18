db245b8f9f5d67846767921fabfd29caeb704248
@ProfNandaa <profnandaa@gmail.com>
2015-03-18 03:17:17 +0300

    Functions enhanced

--- examples/05-functions.py
+++ examples/05-functions.py
@@ -12,3 +12,35 @@ def factor(a):
             return ((a / d), d)
         d = d + 1
     return (a, 1)
+
+#Decorators
+@trace
+def square(x):
+    return x*x
+
+def square(x):
+    return x*x
+square = trace(square)
+
+#Yield
+def countdown(n):
+    print("Counting down from %d" % n)
+    while n > 0:
+        yield n
+    n -= 1
+    return
+
+for n in countdown(10):
+    statements
+a = sum(countdown(10))
+
+#Lambda
+a = lambda x,y : x+y
+r = a(2,3)
+
+#Recursion
+def factorial(n):
+    if n <= 1: return 1
+    else: return n * factorial(n - 1)
+
+#Look for other recursion examples:
